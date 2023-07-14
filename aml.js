12b074c042e4e61dc19adee22555c4bc57f8d244
swetakhadanga <31290680+swetakhadanga@users.noreply.github.com>
2023-07-14 17:57:04 +0200

    Update aml.js

--- aml.js
+++ aml.js
@@ -1,30 +1,24 @@
-// import { myButton } from "/examples/Button/example.js";
-// import { myAutoComplete } from "./examples/AutoComplete/example.js";
-//import { myDropDown } from "./examples/DropDown/example.js";
-//import { myTextInput } from "./examples/TextInput/example.js";
-// import { myDate } from "./examples/DateTime/example.js";
-// import { myDataGrid } from "./examples/DataGrid/example.js";
-//import { myRepeater } from "/obvia/examples/Repeater/example.js";
-//import { loader } from "/obvia/examples/Link/example.js";
-//import { ArrayEx } from "/obvia/lib/ArrayEx.js";
-// import { myAutoBrowser } from "./examples/Autobrowse/example.js";
+ //import { myButton } from "/examples/Button/example.js";
+ // import { myAutoComplete } from "./examples/AutoComplete/example.js";
+ //import { myDropDown } from "./examples/DropDown/example.js";
+ //import { myTextInput } from "./examples/TextInput/example.js";
+ // import { myDate } from "./examples/DateTime/example.js";
+ // import { myDataGrid } from "./examples/DataGrid/example.js";
+ //import { myRepeater } from "/obvia/examples/Repeater/example.js";
+ //import { loader } from "/obvia/examples/Link/example.js";
+ //import { ArrayEx } from "/obvia/lib/ArrayEx.js";
+ // import { myAutoBrowser } from "./examples/Autobrowse/example.js";
 
-//import { mySideNav } from "/obvia/examples/SideNav/example.js";
-import { myHeaderNavBar } from "/obvia/examples/NavBar/example.js";
-//import { loader } from "/obvia/examples/Image/example.js";
+ //import { mySideNav } from "/obvia/examples/SideNav/example.js";
+ import { myNavBar } from "/obvia/examples/NavBar/example.js";
+ import { rjs } from "/obvia/lib/rjs.js";
 
-
-import { rjs } from "/obvia/lib/rjs.js";
-
-rjs.define("/obvia/lib/yaml.js", "yaml");
-rjs.define("/examples/NavBar/example.js", "myHeaderNavBar");
-// rjs.define("/examples/Button/example.js", "myButton");
-//rjs.define("/examples/DropDown/example.js", "myDropDown");
-//rjs.define("/examples/SideNav/example.js", "mySideNav");
-//rjs.define("/examples/Repeater/example.js", "myRepeater");
-//rjs.define("/examples/Link/example.js", "loader");
-//rjs.define("/obvia/lib/ArrayEx.js", "ArrayEx");
+ rjs.define("/obvia/lib/yaml.js", "yaml");
+ //rjs.define("/examples/Rating/example.js", "myRating");
+ rjs.define("/examples/NavBar/example.js", "myNavBar");
+ //rjs.define("/obvia/lib/ArrayEx.js", "ArrayEx");
+ //rjs.define("/examples/Button/example.js", "myButton");
 
 
 
-//import generateApi from "/obvia/examples/ApiClient/example.js";
+ //import generateApi from "/obvia/examples/ApiClient/example.js";
